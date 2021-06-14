faab0a142fac8238ac0ef89d40f654302627772a
gabsgc <gabriellagarccia@gmail.com>
2021-06-14 18:05:32 -0300

    up index, create product.html and get via ajax

--- application/controller/index_controller.py
+++ application/controller/index_controller.py
@@ -1,6 +1,13 @@
 from application import app
-from flask import render_template
+from flask import render_template, request
+from application.model.dao.product_dao import ProductDAO
 
-@app.route('/')
+product_list = ProductDAO().findAll()
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route("/products", methods=['GET'])
+def products():
+    return render_template('product.html', product_list=product_list)
